df411ccd375d334e84597df633aa62f09c09c814
Hiwot1986 <153129450+Hiwot1986@users.noreply.github.com>
2024-02-01 02:04:13 -0500

    netflix clone is committed

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Banner from "../../Components/Banner/Banner";
+import Footer from "../../Components/Footer/Footer";
+import Header from '../../Components/Header/Header';
+import RowList from '../../Components/Rows/RowList/rowlist';
+
+const Home = () => {
+	return (
+		<>
+           <Header />
+			<Banner />
+            <RowList/>
+			<Footer />
+		</>
+	);
+};
+
+export default Home;
